5666e747adafbc9e7f653997cb4b51594be67c98
MarianoGomesOpitz <marianogomesopitz@gmail.com>
2022-09-09 18:43:25 -0300

    Punto 1 y 2

---
+++ LogTest/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using NLog;
+
+
+namespace LogTest // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger log = LogManager.GetCurrentClassLogger();
+
+            log.Trace("Mensaje de Trace");
+            log.Debug("Mensaje de Debug");
+            log.Info("Mensaje de Info");
+            log.Warn("Mensaje de Warn");
+            log.Error("Mensaje de Error");
+            log.Fatal("Mensaje de Fatal");
+        }
+    }
+}
